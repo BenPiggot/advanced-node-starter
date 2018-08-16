a8ff9347def1ac26bd9471463b82ffa438994ba1
ben <wbpiggot@gmail.com>
2018-08-15 21:19:48 -0700

    more work with caching

--- services/cache.js
+++ services/cache.js
@@ -5,9 +5,17 @@ const redisUrl = 'redis://127.0.0.1:6379';
 const client = redis.createClient(redisUrl);
 client.get = util.promisify(client.get)
 
+mongoose.Query.prototype.cache = function() {
+  this.useCache = true;
+  return this;
+}
+
 const exec = mongoose.Query.prototype.exec;
 
 mongoose.Query.prototype.exec = async function() {
+  if (!this.useCache) {
+    return exec.apply(this, arguments);
+  }
 
   const key = JSON.stringify(Object.assign({}, this.getQuery(), 
     { collection: this.mongooseCollection.name }
