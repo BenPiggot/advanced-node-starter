05f1122417cd748203f8bf5aafab9b182d6a5da3
ben <wbpiggot@gmail.com>
2018-08-15 17:10:00 -0700

    more work on caching

--- services/cache.js
+++ services/cache.js
@@ -1,20 +1,26 @@
 const mongoose = require('mongoose');
 const redis = require('redis');
+const util = require('util');
 const redisUrl = 'redis://127.0.0.1:6379';
-const client = redis.createClient(redisUrl)
+const client = redis.createClient(redisUrl);
+client.get = util.promisify(client.get)
 
 const exec = mongoose.Query.prototype.exec;
 
-mongoose.Query.prototype.exec = function() {
+mongoose.Query.prototype.exec = async function() {
   console.log('About to run a query');
-  const query = this.getQuery();
-  const cachedQuery = Object.assign({}, query, { collection: this.mongooseCollection.name })
 
-  // const cachedResult = client.get(cachedQuery);
-  // if (cachedResult) {
-  //   return cachedResult;
-  // }
+  const key = JSON.stringify(Object.assign({}, this.getQuery(), 
+    { collection: this.mongooseCollection.name }
+  ));
+  const cachedResult = await client.get(key);
+  
+  if (cachedResult) {
+    console.log(cachedResult)
+    return JSON.parse(cachedResult)
+  }
 
-  // client.selected_db()
-  return exec.apply(this, arguments);
+  const result = await exec.apply(this, arguments);
+  await client.set(key, JSON.stringify(result));
+  return result;
 }
